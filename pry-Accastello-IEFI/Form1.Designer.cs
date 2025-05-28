8561e0ddeea981942563602997489947f23f011e
andresaccastello <andres.soltec2@gmail.com>
2025-05-28 15:29:03 -0300

    Cambios

--- pry-Accastello-IEFI/Form1.Designer.cs
+++ pry-Accastello-IEFI/Form1.Designer.cs
@@ -29,22 +29,20 @@
         private void InitializeComponent()
         {
             this.gpLogin = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnIngresar = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.gpLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // gpLogin
             // 
-            this.gpLogin.Controls.Add(this.button1);
             this.gpLogin.Controls.Add(this.btnIngresar);
-            this.gpLogin.Controls.Add(this.textBox2);
-            this.gpLogin.Controls.Add(this.textBox1);
+            this.gpLogin.Controls.Add(this.txtContraseña);
+            this.gpLogin.Controls.Add(this.txtUsuario);
             this.gpLogin.Controls.Add(this.label2);
             this.gpLogin.Controls.Add(this.label1);
             this.gpLogin.Location = new System.Drawing.Point(119, 83);
@@ -54,14 +52,30 @@
             this.gpLogin.TabStop = false;
             this.gpLogin.Text = "LOGIN";
             // 
-            // label1
+            // btnIngresar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario";
+            this.btnIngresar.Location = new System.Drawing.Point(129, 147);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(147, 82);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(147, 42);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 2;
             // 
             // label2
             // 
@@ -72,39 +86,14 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña";
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(147, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(147, 82);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
-            // 
-            // btnIngresar
-            // 
-            this.btnIngresar.Location = new System.Drawing.Point(52, 146);
-            this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
-            this.btnIngresar.TabIndex = 4;
-            this.btnIngresar.Text = "Ingresar";
-            this.btnIngresar.UseVisualStyleBackColor = true;
-            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(212, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Registrarse";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
             // 
             // frmLogin
             // 
@@ -114,6 +103,7 @@
             this.Controls.Add(this.gpLogin);
             this.Name = "frmLogin";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
             this.gpLogin.ResumeLayout(false);
             this.gpLogin.PerformLayout();
             this.ResumeLayout(false);
@@ -124,12 +114,11 @@
 
         private System.Windows.Forms.GroupBox gpLogin;
         private System.Windows.Forms.Button btnIngresar;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- pry-Accastello-IEFI/Form1.cs
+++ pry-Accastello-IEFI/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,56 @@ namespace pry_Accastello_IEFI
         {
             InitializeComponent();
         }
-
+        int intentosFallidos = 0;
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            frmInicio ini = new frmInicio();
-            ShowDialog(ini);
+            string connectionString = "Server=localhost;Database=Auditoria;Trusted_Connection=True";
+            string username = txtUsuario.Text;
+            string password = txtContraseña.Text;
+            
+
+            string query = "SELECT COUNT(*) FROM Usuarios WHERE Nombre = @nombre AND Contraseña = @contraseña";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@nombre", username);
+                    cmd.Parameters.AddWithValue("@contraseña", password);
+
+                    int count = (int)cmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Inicio de sesión exitoso.");
+                        Form productos = new frmInicio();
+                        productos.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        intentosFallidos++; // Aumentar el contador
+                        MessageBox.Show("Usuario o contraseña incorrectos. Intento " + intentosFallidos + " de 3.");
+
+                        if (intentosFallidos >= 3)
+                        {
+                            MessageBox.Show("Demasiados intentos fallidos. El formulario se cerrará.");
+                            this.Close(); // Cierra el formulario actual
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
